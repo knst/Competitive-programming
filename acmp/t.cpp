c5cd4390d008228d7effe53a86bc70be67583611
Konstantin Akimov <knstqq@gmail.com>
2015-05-07 00:48:45 +0300

    Added template for acmp.

---
+++ acmp/t.cpp
@@ -0,0 +1,45 @@
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
+
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+#ifdef _KNST
+    istream& input = cin;
+    ostream& output = cout;
+#else
+    ifstream input("INPUT.TXT");
+    ofstream output("OUTPUT.TXT");
+#endif
+
+
+
+    return 0;
+}
