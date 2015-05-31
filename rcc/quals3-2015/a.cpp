68c3e19e48430949b43255f818229e6800ca0a99
Konstantin Akimov <knstqq@gmail.com>
2015-06-01 02:43:14 +0300

    Added solution of problem A, B for Russian Code Cup 2015 Quals 3.

---
+++ rcc/quals3-2015/a.cpp
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+namespace std {
+    template <class T1, class T2>
+    class hash<pair<T1, T2>> {
+    public:
+        size_t operator()(const pair<T1, T2>& p) const {
+            return hash<T1>()(p.first) & hash<T2>()(p.second);
+        }
+    };
+}
+
+constexpr long double const_pi() { return std::atan(static_cast<long double>(1))*4; }
+
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
+
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    size_t t;
+    cin >> t;
+    while (t--) {
+        int a;
+        int b;
+        int c;
+        cin >> a >> b >> c;
+        bool ok = true;
+        if (c % 2 && a == 0)
+            ok = false;
+        if (c % 2)
+            --a, --c;
+        if (b * 2 + a < c)
+            ok = false;
+        if (ok)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
+    }
+
+
+
+    return 0;
+}
