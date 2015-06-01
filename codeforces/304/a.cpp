00d54c0e29f258ff162c6578cccb0451f1977697
Konstantin Akimov <knstqq@gmail.com>
2015-06-01 18:06:21 +0300

    Added solution of problems A and B for Codeforces Round 304.

---
+++ codeforces/304/a.cpp
@@ -0,0 +1,57 @@
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+namespace std {
+    template <class T1, class T2>
+    class hash<pair<T1, T2>> {
+    public:
+        size_t operator()(const pair<T1, T2>& p) const {
+            return hash<T1>()(p.first) & hash<T2>()(p.second);
+        }
+    };
+}
+
+constexpr long double const_pi() { return std::atan(static_cast<long double>(1))*4; }
+
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
+
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    int64_t k;
+    int64_t n;
+    int64_t w;
+    cin >> k >> n >> w;
+    int64_t sum = (1 + w) * w / 2 * k;
+    sum -= n;
+    if (sum < 0)
+        sum = 0;
+    cout << sum;
+
+
+    return 0;
+}
