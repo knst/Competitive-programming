6cda99305cdc646c5f28026e39e923ee1fb5865e
Konstantin Akimov <knstqq@gmail.com>
2015-06-11 17:51:36 +0300

    Added solution of problem G for looksery round on CF.

---
+++ codeforces/looksery-2015/g.cpp
@@ -0,0 +1,63 @@
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+namespace std {
+    template <class T1, class T2>
+    class hash<pair<T1, T2>> {
+    public:
+        size_t operator()(const pair<T1, T2>& p) const {
+            return hash<T1>()(p.first) & hash<T2>()(p.second);
+        }
+    };
+}
+
+constexpr long double const_pi() { return std::atan(static_cast<long double>(1))*4; }
+
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
+
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    size_t n;
+    cin >> n;
+    vector<int> v(n);
+    for (size_t i = 0; i < n; ++i) {
+        cin >> v[i];
+        v[i] += static_cast<int>(i);
+    }
+    sort(v.begin(), v.end());
+    for (size_t i = 1; i < n; ++i)
+        if (v[i] - v[i-1] == 0) {
+            cout << ":(" << endl;
+            return 0;
+        }
+    for (size_t i = 0; i < n; ++i)
+        cout << v[i] -  i << ' ';
+    cout << endl;
+    return 0;
+}
