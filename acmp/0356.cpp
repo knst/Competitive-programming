8ff90c2c3251faef73087d006450c338427123a1
Konstantin Akimov <knstqq@gmail.com>
2015-07-22 22:23:17 +0300

    Replaced solution of problem #356 for acmp to non-minimized version.

--- acmp/0356.cpp
+++ acmp/0356.cpp
@@ -1,23 +1,76 @@
+
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <cmath>
 #include <fstream>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
 
-int e, f, w, p, j, B[9999], A[9999];
+using namespace std;
 
-main() {
-    std::fstream I("INPUT.TXT");
-    std::ofstream O("OUTPUT.TXT");
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
 
-    for (
-        I >> e >> f >> p,
-        f-= e;
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
 
-        I >> p >> w;
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
 
-    )
-        for (j = w; e = B[j - w] + p, j <= f; ++j)
-            if (j == w || e - p)
-                B[j] < e && (B[j] = e),
-                (e = A[j - w] + p) > A[j] && A[j] || (A[j] = e);
-    !f | B[f]
-        ? O << A[f] << ' ' << B[f]
-        : O << "This is impossible.";
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+#ifdef _KNST
+    istream& input = cin;
+    ostream& output = cout;
+#else
+    ifstream input("INPUT.txt");
+    ofstream output("OUTPUT.txt");
+#endif
+
+    size_t e;
+    size_t f;
+    input >> e >> f;
+    f -= e;
+    size_t n;
+    input >> n;
+
+    vector<size_t> maxcost(f + 1);
+    vector<size_t> mincost(f + 1);
+
+    for (size_t i = 0; i < n; ++i) {
+        size_t w;
+        size_t p;
+        input >> p >> w;
+        for (size_t j = 0; j + w <= f; ++j) {
+            if (j == 0 || maxcost[j]) {
+                maxcost[j + w] = max(maxcost[j + w], maxcost[j] + p);
+                if (mincost[j + w])
+                    mincost[j + w] = min(mincost[j + w], mincost[j] + p);
+                else
+                    mincost[j + w] = mincost[j] + p;
+            }
+        }
+    }
+    if (f == 0 || maxcost[f])
+        output << mincost[f] << ' ' << maxcost[f] << endl;
+    else
+        output << "This is impossible." << endl;
+
+
+    return 0;
 }
