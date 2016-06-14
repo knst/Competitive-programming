bfdea1c585144dcb4b7dda7ccc22c0a6765eb014
Konstantin Akimov <knstqq@gmail.com>
2016-06-14 12:29:21 +0700

    Added solution of A for Yandex algorithm 2016, round 3.

---
+++ sponsored/yandex/round3-2016/a.cpp
@@ -0,0 +1,67 @@
+/*****************************************************************************
+ * codeforces:   knst
+ * topcoder:     knstqq
+ * projecteuler: knstqq
+ * **************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+namespace std {
+    template <class T1, class T2>
+    class hash<pair<T1, T2>> {
+    public:
+        size_t operator()(const pair<T1, T2>& p) const {
+            return hash<T1>()(p.first) & hash<T2>()(p.second);
+        }
+    };
+}
+
+constexpr long double const_pi() { return std::atan(static_cast<long double>(1))*4; }
+
+const int dx4[] = {-1, 0, 0, 1};
+const int dy4[] = {0, -1, 1, 0};
+
+const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+
+    size_t n;
+    cin >> n;
+    int left = numeric_limits<int>::max();
+    int right= numeric_limits<int>::min();
+    for (size_t i = 0; i < n; ++i) {
+        int value;
+        char sign;
+        cin >> value >> sign;
+        if (sign == '-')
+            left = min(left, value);
+        else
+            right = max(right, value);
+    }
+    if (left < right) {
+        cout << 2 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+
+
+    return 0;
+}
