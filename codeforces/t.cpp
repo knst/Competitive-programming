1744061a4f4e338c31c927594ed7c989e58a488b
Konstantin Akimov <knstqq@gmail.com>
2016-09-04 19:12:11 +0700

    Improved codeforces template: writed more compact.

--- codeforces/t.cpp
+++ codeforces/t.cpp
@@ -17,8 +17,7 @@ using namespace std;
 
 namespace std {
     template <class T1, class T2>
-    class hash<pair<T1, T2>> {
-    public:
+    struct hash<pair<T1, T2>> {
         size_t operator()(const pair<T1, T2>& p) const {
             return hash<T1>()(p.first) & hash<T2>()(p.second);
         }
@@ -34,9 +33,7 @@ const int dx8[] = {-1, -1, -1,  0,  0,  1,  1,  1};
 const int dy8[] = {-1,  0,  1, -1,  1, -1,  0,  1};
 
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 int main() {
